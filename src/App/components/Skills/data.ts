dae115fc2b40a72fbd3531639025ffb96407aa1f
iambenbright <yankeybenneth@gmail.com>
2021-01-28 14:37:11 +0000

    feat: adds skills section

---
+++ src/App/components/Skills/data.ts
@@ -0,0 +1,36 @@
+import { CodeSVG } from './CodeSVG';
+import { ResponsiveSVG } from './ResponsiveSVG';
+import { TeachSVG } from './TeachSVG';
+
+export interface IData {
+  id: number;
+  media: () => JSX.Element;
+  header: string;
+  body: string;
+}
+
+const data: IData[] = [
+  {
+    id: 1,
+    media: CodeSVG,
+    header: 'Design To HTML',
+    body:
+      "Do you have nice design in PSD, Figma or Sketch?, contact me,I'll convert them to fully responsive websites",
+  },
+  {
+    id: 2,
+    media: ResponsiveSVG,
+    header: 'Responsive Web',
+    body:
+      'With the rise of mobile devices, you website must be able to look good on these varied devices',
+  },
+  {
+    id: 3,
+    media: TeachSVG,
+    header: 'Mentor',
+    body:
+      'I genuinely care about other developers, and help them with their craft',
+  },
+];
+
+export { data };
---
+++ src/App/components/Skills/index.tsx
@@ -0,0 +1,74 @@
+// material components
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Container from '@material-ui/core/Container';
+
+// material utils
+import { makeStyles, Theme, createStyles } from '@material-ui/core';
+
+// data
+import { data, IData } from './data';
+
+// styles
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      padding: theme.spacing(10, 0, 0),
+      textAlign: 'center',
+      '& > *:not(:last-child)': {
+        marginBottom: theme.spacing(4),
+      },
+    },
+    card: {
+      backgroundColor: 'transparent',
+      padding: theme.spacing(5, 0),
+    },
+    cardHeader: {
+      fontWeight: 'bold',
+    },
+    cardContent: {
+      marginTop: theme.spacing(3),
+    },
+  })
+);
+
+function Skill({ id, media, header, body }: IData) {
+  const classes = useStyles();
+
+  return (
+    <Grid key={id} item sm={6} md={4}>
+      <Card elevation={0} className={classes.card}>
+        <CardMedia>{media()}</CardMedia>
+        <CardContent>
+          <Typography variant="subtitle1" className={classes.cardHeader}>
+            {header}
+          </Typography>
+          <Typography className={classes.cardContent}>{body}</Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+}
+
+function Skills() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <Container>
+        <Typography variant="h5">What I Can Do</Typography>
+        <Typography>Here are all the services I can offer</Typography>
+        <Grid container spacing={1} style={{ marginTop: 48 }}>
+          {data.map(item => (
+            <Skill key={item.id} {...item} />
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+export default Skills;
