ab52265d7f1f3314d07782ecc15f48556d1faacd
iambenbright <yankeybenneth@gmail.com>
2021-02-06 20:14:58 +0000

    feat: adds interface properties

--- src/App/components/Portfolio/data.ts
+++ src/App/components/Portfolio/data.ts
@@ -1,15 +1,18 @@
 // portfolio images
-import restCountries from '../../assets/images/rest-countries.png';
-import bookmark from '../../assets/images/bookmark.png';
-import easyBank from '../../assets/images/easy-bank.png';
-import managed from '../../assets/images/managed.png';
-import socialDashboard from '../../assets/images/social-dashboard.png';
+import restCountries from '../../assets/images/rest-countries.webp';
+import bookmark from '../../assets/images/bookmark.webp';
+import easyBank from '../../assets/images/easy-bank.webp';
+import managed from '../../assets/images/managed.webp';
+import socialDashboard from '../../assets/images/social-dashboard.webp';
 
 export interface ImageInterface {
   src: string;
   title: string;
   alt: string;
   summary: string;
+  stack: string;
+  demo: string;
+  code: string;
 }
 
 // images
@@ -18,35 +21,52 @@ const images: ImageInterface[] = [
     src: bookmark,
     title: 'bookmark',
     alt: 'bookmark',
-    summary:
-      'Bookmark helps manage your favourite sites. A landing page created with React, Typescript & Material-UI',
+    summary: 'Bookmark helps manage your favourite sites',
+    stack: 'React, Typescript & Material-UI',
+    demo: 'https://bookmark-landing-page-six-lake.vercel.app/',
+    code: 'https://github.com/iambenbright/bookmark',
   },
   {
     src: managed,
     title: 'managed',
     alt: 'managed',
-    summary:
-      'Managed makes it simple for teams to plan day-to-day tasks. A landing page created with React & Material-UI',
+    summary: 'Managed makes it simple for teams to plan day-to-day tasks',
+    stack: 'React & Material-UI',
+    demo: 'https://managed-landing.vercel.app/',
+    code: 'https://github.com/iambenbright/managed',
   },
   {
     src: restCountries,
     title: 'rest-countries',
     alt: 'rest-countries',
-    summary: '',
+    summary:
+      'Get information about countries including population, currency etc',
+    stack: 'React & Styled-Component',
+    demo: 'https://countries-of-the-world.vercel.app/',
+    code: 'https://github.com/iambenbright/where-in-the-world',
   },
   {
     src: easyBank,
     title: 'easy-bank',
     alt: 'easy-bank',
     summary:
-      'EasyBank account is one-stop-shop for spending, saving etc online. A landing page created with HTML5, CSS3 & JavaScript ',
+      'EasyBank account is one-stop-shop for spending, saving etc online',
+    stack: 'HTML5, CSS3 & JavaScript',
+    demo:
+      'https://iambenbright.github.io/frontend-mentor-projects/easy-bank-landing-page/',
+    code:
+      'https://github.com/iambenbright/frontend-mentor-projects/tree/master/easy-bank-landing-page',
   },
   {
     src: socialDashboard,
     title: 'social-dashboard',
     alt: 'social-dashboard',
-    summary:
-      'Manage your social life with all-in-one social media dashboard. A web app created with HTML5, CSS3 & JavaScript',
+    summary: 'Manage your social life with all-in-one social media dashboard.',
+    stack: 'HTML5, CSS3 & JavaScript',
+    demo:
+      'https://iambenbright.github.io/frontend-mentor-projects/social-media-dashboard/',
+    code:
+      'https://github.com/iambenbright/frontend-mentor-projects/tree/master/social-media-dashboard',
   },
 ];
 
