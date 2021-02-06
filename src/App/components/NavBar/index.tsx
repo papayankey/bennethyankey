e9d03b062ef58ee38dacf35bfab0835e1a21dc2c
iambenbright <yankeybenneth@gmail.com>
2021-02-06 20:20:54 +0000

    chore: refactor nav links

--- src/App/components/NavBar/index.tsx
+++ src/App/components/NavBar/index.tsx
@@ -1,16 +1,14 @@
 // material components
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Link from '@material-ui/core/Link';
 import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 
-// components
-import {
-  makeStyles,
-  createStyles,
-  Theme,
-  useScrollTrigger,
-} from '@material-ui/core';
+// material-utils
+import { makeStyles, createStyles, Theme } from '@material-ui/core';
+
+// component
+import { Link } from 'react-scroll';
 
 // styles
 const useStyles = makeStyles((theme: Theme) =>
@@ -19,37 +17,62 @@ const useStyles = makeStyles((theme: Theme) =>
       backgroundColor: theme.palette.background.default,
     },
     toolbar: {
-      padding: theme.spacing(5, 0),
-      justifyContent: 'center',
-      '& a': {
-        color: 'black',
-        fontSize: '1rem',
-        letterSpacing: '1.25px',
-        '&:not(:last-of-type)': {
-          marginRight: theme.spacing(4),
-        },
-      },
+      justifyContent: 'flex-end',
+    },
+    anchor: {
+      color: theme.palette.text.primary,
+      textTransform: 'uppercase',
+      fontSize: '1rem',
+      fontWeight: 400,
+      letterSpacing: '1.25px',
+      marginRight: theme.spacing(4),
+    },
+    active: {
+      fontWeight: 700,
+      color: theme.palette.primary.main,
     },
   })
 );
 
+// navlinks
+const navLinks = ['home', 'about', 'skills', 'portfolio'];
+
 function NavBar() {
   const classes = useStyles();
-  const scrollTrigger = useScrollTrigger({ threshold: 0 });
 
   return (
-    <AppBar
-      elevation={scrollTrigger ? 1 : 0}
-      position="fixed"
-      className={classes.appBar}
-    >
+    <AppBar elevation={0} position="fixed" className={classes.appBar}>
       <Container>
         <Toolbar className={classes.toolbar}>
-          <Link href="#home">Home</Link>
-          <Link href="#about">About</Link>
-          <Link href="#skills">Skills</Link>
-          <Link href="#portfolio">Portfolio</Link>
-          <Link href="#contact">Contact</Link>
+          <nav>
+            {navLinks.map((link, idx) => {
+              let offset = -200;
+              if (link === 'portfolio') {
+                offset = -150;
+              }
+              if (link === 'contact') {
+                offset = -100;
+              }
+              return (
+                <Link
+                  key={idx}
+                  activeClass={classes.active}
+                  to={link}
+                  smooth={true}
+                  spy={true}
+                  offset={offset}
+                  className={classes.anchor}
+                >
+                  {link}
+                </Link>
+              );
+            })}
+            <Button variant="contained" size="large" color="secondary">
+              <Link to="contact" smooth={true} spy={true} offset={200}>
+                Let's Connect
+              </Link>
+            </Button>
+          </nav>
         </Toolbar>
       </Container>
     </AppBar>
