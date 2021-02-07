f2273ddfd15b8108fba231b0241950714d45900b
iambenbright <yankeybenneth@gmail.com>
2021-02-07 13:27:18 +0000

    feat: adds bg color to icons

--- src/App/components/Skills/index.tsx
+++ src/App/components/Skills/index.tsx
@@ -5,9 +5,10 @@ import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 
 // material utils
-import { makeStyles, Theme, createStyles } from '@material-ui/core';
+import { makeStyles, Theme, createStyles, useTheme } from '@material-ui/core';
 
 // components
 import { Layout } from '../../common';
@@ -21,17 +22,30 @@ const useStyles = makeStyles((theme: Theme) =>
     container: {
       padding: theme.spacing(10, 0, 0),
     },
+    title: {
+      color: theme.palette.primary.main,
+    },
+    gridContainer: {
+      paddingBottom: theme.spacing(10),
+      paddingTop: theme.spacing(5),
+    },
     card: {
-      backgroundColor: 'transparent',
-      padding: theme.spacing(10, 3),
+      backgroundColor: theme.palette.common.white,
+      padding: theme.spacing(6, 3),
+      height: '100%',
+    },
+    cardMedia: {
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    cardContent: {
+      padding: 0,
+      marginTop: theme.spacing(3),
     },
     cardHeader: {
       fontWeight: 700,
     },
-    title: {
-      color: theme.palette.primary.main,
-    },
-    cardContent: {
+    cardBody: {
       marginTop: theme.spacing(3),
     },
   })
@@ -39,14 +53,28 @@ const useStyles = makeStyles((theme: Theme) =>
 
 function Skill({ id, media, header, body }: IData) {
   const classes = useStyles();
+  const theme = useTheme();
 
   return (
     <Grid key={id} item sm={6} md={4}>
-      <Card elevation={0} className={classes.card}>
-        <CardMedia>{media()}</CardMedia>
-        <CardContent>
+      <Card variant="outlined" className={classes.card}>
+        <CardMedia className={classes.cardMedia}>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            width={100}
+            height={100}
+            borderRadius="50px"
+            bgcolor={theme.palette.primary.light}
+            boxShadow="inset 0px 0px 4px #1d81791f"
+          >
+            {media}
+          </Box>
+        </CardMedia>
+        <CardContent className={classes.cardContent}>
           <Typography className={classes.cardHeader}>{header}</Typography>
-          <Typography className={classes.cardContent}>{body}</Typography>
+          <Typography className={classes.cardBody}>{body}</Typography>
         </CardContent>
       </Card>
     </Grid>
@@ -64,7 +92,7 @@ function Skills() {
             What I can do
           </Typography>
           <Typography>Here are all the services I can offer</Typography>
-          <Grid container spacing={5}>
+          <Grid container spacing={5} className={classes.gridContainer}>
             {data.map(item => (
               <Skill key={item.id} {...item} />
             ))}
