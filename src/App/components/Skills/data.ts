bd593aff6b15ade335ae49ad110584276b455966
iambenbright <yankeybenneth@gmail.com>
2021-02-11 12:37:14 +0000

    feat: replace icons with material icons

--- src/App/components/Skills/data.ts
+++ src/App/components/Skills/data.ts
@@ -1,30 +1,38 @@
-import { CodeSVG, ResponsiveSVG, TeachSVG } from './svg';
+// material icons
+import { SvgIconComponent } from '@material-ui/icons';
+
+// material components
+import DeveloperBoardIcon from '@material-ui/icons/DeveloperBoardOutlined';
+import LanguageIcon from '@material-ui/icons/LanguageOutlined';
+import LaptopIcon from '@material-ui/icons/LaptopMacOutlined';
 
 export interface IData {
   id: number;
-  media: JSX.Element;
+  Icon: SvgIconComponent;
   header: string;
-  body: string;
+  body: string | string[];
 }
 
 const data: IData[] = [
   {
     id: 1,
-    media: CodeSVG(),
+    Icon: DeveloperBoardIcon,
     header: 'Design To HTML',
-    body:
-      "Do you have nice design in PSD, Figma or Sketch?, contact me,I'll convert them to fully responsive websites",
+    body: [
+      'Do you have nice design in PSD, Figma or Sketch?, ',
+      ", I'll convert them to fully responsive websites",
+    ],
   },
   {
     id: 2,
-    media: ResponsiveSVG(),
+    Icon: LaptopIcon,
     header: 'Responsive Web',
     body:
       'With the rise of mobile devices, you website must be able to look good on these varied devices',
   },
   {
     id: 3,
-    media: TeachSVG(),
+    Icon: LanguageIcon,
     header: 'Mentor',
     body:
       'I genuinely care about other developers, and help them with their craft',
--- src/App/components/Skills/svg/index.ts
+++
@@ -1,3 +0,0 @@
-export { CodeSVG } from './CodeSVG';
-export { ResponsiveSVG } from './ResponsiveSVG';
-export { TeachSVG } from './TeachSVG';
