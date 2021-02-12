5b1756832b77a97e17b73ff6fbd50d1be2e3165a
iambenbright <yankeybenneth@gmail.com>
2021-02-12 12:28:55 +0000

    feat: replace svg icons

--- src/App/components/Skills/data.ts
+++ src/App/components/Skills/data.ts
@@ -2,9 +2,9 @@
 import { SvgIconComponent } from '@material-ui/icons';
 
 // material components
-import DeveloperBoardIcon from '@material-ui/icons/DeveloperBoardOutlined';
-import LanguageIcon from '@material-ui/icons/LanguageOutlined';
-import LaptopIcon from '@material-ui/icons/LaptopMacOutlined';
+import CodeIcon from '@material-ui/icons/CodeOutlined';
+import AssignmentIcon from '@material-ui/icons/AssignmentOutlined';
+import DevicesIcon from '@material-ui/icons/DevicesOutlined';
 
 export interface IData {
   id: number;
@@ -16,7 +16,7 @@ export interface IData {
 const data: IData[] = [
   {
     id: 1,
-    Icon: DeveloperBoardIcon,
+    Icon: CodeIcon,
     header: 'Design To HTML',
     body: [
       'Do you have nice design in PSD, Figma or Sketch?, ',
@@ -25,14 +25,14 @@ const data: IData[] = [
   },
   {
     id: 2,
-    Icon: LaptopIcon,
+    Icon: DevicesIcon,
     header: 'Responsive Web',
     body:
       'With the rise of mobile devices, you website must be able to look good on these varied devices',
   },
   {
     id: 3,
-    Icon: LanguageIcon,
+    Icon: AssignmentIcon,
     header: 'Mentor',
     body:
       'I genuinely care about other developers, and help them with their craft',
