9e2379ff1e33f9201e72b13e087dc6763e3c54a3
iambenbright <yankeybenneth@gmail.com>
2021-02-14 11:14:57 +0000

    chore: adds back scroll trigger

--- src/App/components/NavBar/index.tsx
+++ src/App/components/NavBar/index.tsx
@@ -5,7 +5,12 @@ import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 
 // material-utils
-import { makeStyles, createStyles, Theme } from '@material-ui/core';
+import {
+  makeStyles,
+  createStyles,
+  Theme,
+  useScrollTrigger,
+} from '@material-ui/core';
 
 // component
 import { Link } from 'react-scroll';
@@ -14,13 +19,13 @@ import { Link } from 'react-scroll';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     appBar: {
-      backgroundColor: theme.palette.background.default,
+      backgroundColor: theme.palette.common.white,
     },
     toolbar: {
       justifyContent: 'flex-end',
     },
     anchor: {
-      color: theme.palette.text.primary,
+      color: theme.palette.grey[600],
       textTransform: 'uppercase',
       fontSize: '1rem',
       fontWeight: 400,
@@ -35,28 +40,30 @@ const useStyles = makeStyles((theme: Theme) =>
       fontWeight: 700,
       color: theme.palette.primary.main,
     },
+    addShadow: {
+      boxShadow: '0 1px 1px rgba(0,0,0,0.05)',
+    },
   })
 );
 
-// navlinks
+// constants
 const navLinks = ['home', 'about', 'skills', 'portfolio'];
+const offset = -150;
 
 function NavBar() {
-  const classes = useStyles();
+  const trigger = useScrollTrigger({ disableHysteresis: true, threshold: 1 });
+  const classes = useStyles(trigger);
 
   return (
-    <AppBar elevation={0} position="fixed" className={classes.appBar}>
+    <AppBar
+      elevation={0}
+      position="fixed"
+      className={`${classes.appBar} ${trigger ? classes.addShadow : ''}`}
+    >
       <Container>
         <Toolbar className={classes.toolbar}>
           <nav>
             {navLinks.map((link, idx) => {
-              let offset = -200;
-              if (link === 'portfolio') {
-                offset = -150;
-              }
-              if (link === 'contact') {
-                offset = -100;
-              }
               return (
                 <Link
                   key={idx}
@@ -72,7 +79,7 @@ function NavBar() {
               );
             })}
             <Button variant="contained" size="large" color="secondary">
-              <Link to="contact" smooth={true} spy={true} offset={200}>
+              <Link to="contact" smooth={true} spy={true} offset={offset}>
                 Let's Connect
               </Link>
             </Button>
