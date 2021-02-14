d1b859e8682add2b73b63fe3476dd736fc9f8e7b
iambenbright <yankeybenneth@gmail.com>
2021-02-14 00:04:51 +0000

    chore: extract email service and styles

--- src/App/components/Contact/Form.tsx
+++ src/App/components/Contact/Form.tsx
@@ -1,88 +1,127 @@
+// react
+import { useState } from 'react';
+
 // material components
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-// material utils
-import { makeStyles, createStyles, Theme } from '@material-ui/core';
+// components
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { emailService } from './emailService';
 
 // styles
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    form: {
-      textAlign: 'left',
-    },
-    button: {
-      marginTop: theme.spacing(4),
-    },
-    textField: {
-      '& .MuiFilledInput-root:hover, & .MuiFilledInput-root.Mui-focused': {
-        backgroundColor: theme.palette.primary.light,
-      },
-      '& .MuiFilledInput-underline::before': {
-        borderBottom: `1px solid #1d81791f`,
-      },
-    },
-  })
-);
+import { useStyles } from './styles';
+
+export type FormData = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+
+// validation schema
+const schema = yup.object().shape({
+  name: yup.string().required(),
+  email: yup.string().email().required(),
+  subject: yup.string().required(),
+  message: yup.string().required(),
+});
 
 function Form() {
+  const { handleSubmit, register, errors, reset } = useForm<FormData>({
+    defaultValues: {
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    },
+    resolver: yupResolver(schema),
+  });
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const classes = useStyles();
 
+  const onSubmit = handleSubmit(data => {
+    emailService(data, setIsSubmitting, reset);
+  });
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={onSubmit} className={classes.form}>
       <TextField
+        inputRef={register}
         className={classes.textField}
         variant="filled"
-        hiddenLabel
         label="Name"
-        type="text"
         name="name"
         placeholder="Enter your name"
-        inputProps={{ 'aria-placeholder': 'Your Name', 'aria-label': 'name' }}
+        error={errors.name && true}
+        inputProps={{
+          'aria-placeholder': 'enter your name',
+          'aria-label': 'name',
+        }}
       />
+      <Typography variant="body2" className={classes.helperText}>
+        {errors.name?.message ?? ''}
+      </Typography>
       <TextField
+        inputRef={register}
         className={classes.textField}
         variant="filled"
-        hiddenLabel
         label="Email"
-        type="email"
         name="email"
-        placeholder="example@gmail.com"
+        placeholder="Enter your email"
+        error={errors.email && true}
         inputProps={{
-          'aria-placeholder': 'Email Address',
+          'aria-placeholder': 'example@gmail.com',
           'aria-label': 'email',
         }}
       />
+      <Typography variant="body2" className={classes.helperText}>
+        {errors.email?.message ?? ''}
+      </Typography>
       <TextField
+        inputRef={register}
         className={classes.textField}
         variant="filled"
-        hiddenLabel
         label="Subject"
-        type="text"
         name="subject"
         placeholder="What does this project involve?"
+        error={errors.subject && true}
         inputProps={{
           'aria-placeholder': 'What does this project involve?',
           'aria-label': 'subject',
         }}
       />
+      <Typography variant="body2" className={classes.helperText}>
+        {errors.subject?.message ?? ''}
+      </Typography>
       <TextField
+        inputRef={register}
         className={classes.textField}
         variant="filled"
-        hiddenLabel
         label="Message"
-        type="text"
         name="message"
         placeholder="Please give detailed description of the project"
+        error={errors.message && true}
         multiline={true}
-        rows={3}
-        inputProps={{ 'aria-placeholder': 'message', 'aria-label': 'message' }}
+        rows={5}
+        inputProps={{
+          'aria-placeholder': 'Please give detailed description of the project',
+          'aria-label': 'message',
+        }}
       />
+      <Typography variant="body2" className={classes.helperText}>
+        {errors.message?.message ?? ''}
+      </Typography>
       <Button
+        type="submit"
         variant="contained"
         size="large"
         color="secondary"
         className={classes.button}
+        disabled={isSubmitting}
       >
         Send Message
       </Button>
