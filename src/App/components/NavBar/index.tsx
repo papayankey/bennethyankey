c1a11fb514feae196d5e481d398cb6226cbe1b47
iambenbright <yankeybenneth@gmail.com>
2021-02-15 21:13:46 +0000

    chore: refactor styles and mobile navigation

--- src/App/components/NavBar/index.tsx
+++ src/App/components/NavBar/index.tsx
@@ -1,92 +1,75 @@
+// react
+import { Fragment, useEffect, useState, useRef } from 'react';
+
 // material components
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Hidden from '@material-ui/core/Hidden';
+import HamburgerIcon from '@material-ui/icons/Menu';
 
-// material-utils
-import {
-  makeStyles,
-  createStyles,
-  Theme,
-  useScrollTrigger,
-} from '@material-ui/core';
+// material utils
+import { useScrollTrigger } from '@material-ui/core';
 
-// component
+// components
 import { Link } from 'react-scroll';
+import { NavLinks } from '../../common';
+import { MobileNavigation } from './Mobile/index';
 
 // styles
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    appBar: {
-      backgroundColor: theme.palette.common.white,
-    },
-    toolbar: {
-      justifyContent: 'flex-end',
-    },
-    anchor: {
-      color: theme.palette.grey[600],
-      textTransform: 'uppercase',
-      fontSize: '1rem',
-      fontWeight: 400,
-      letterSpacing: '1.25px',
-      marginRight: theme.spacing(4),
-      cursor: 'pointer',
-      '&:hover': {
-        color: theme.palette.primary.main,
-      },
-    },
-    active: {
-      fontWeight: 700,
-      color: theme.palette.primary.main,
-    },
-    addShadow: {
-      boxShadow: '0 1px 1px rgba(0,0,0,0.05)',
-    },
-  })
-);
-
-// constants
-const navLinks = ['home', 'about', 'skills', 'portfolio'];
-const offset = -150;
+import { useStyles } from './styles';
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const rootElement = useRef(document.querySelector('#root'))
+    .current as HTMLDivElement;
+
   const trigger = useScrollTrigger({ disableHysteresis: true, threshold: 1 });
-  const classes = useStyles(trigger);
+  const classes = useStyles();
+
+  useEffect(() => {
+    if (menuOpen) {
+      document.body.style['overflow'] = 'hidden';
+      rootElement.style['overflow'] = 'hidden';
+    } else {
+      document.body.style['overflow'] = 'auto';
+      rootElement.style['overflow'] = 'auto';
+    }
+  }, [menuOpen, rootElement]);
 
   return (
-    <AppBar
-      elevation={0}
-      position="fixed"
-      className={`${classes.appBar} ${trigger ? classes.addShadow : ''}`}
-    >
-      <Container>
-        <Toolbar className={classes.toolbar}>
-          <nav>
-            {navLinks.map((link, idx) => {
-              return (
-                <Link
-                  key={idx}
-                  activeClass={classes.active}
-                  to={link}
-                  smooth={true}
-                  spy={true}
-                  offset={offset}
-                  className={classes.anchor}
-                >
-                  {link}
-                </Link>
-              );
-            })}
-            <Button variant="contained" size="large" color="secondary">
-              <Link to="contact" smooth={true} spy={true} offset={offset}>
-                Let's Connect
-              </Link>
-            </Button>
-          </nav>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <Fragment>
+      <MobileNavigation menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <AppBar
+        elevation={0}
+        position="fixed"
+        className={`${classes.appBar} ${trigger ? classes.addShadow : ''}`}
+      >
+        <Container>
+          <Toolbar className={classes.toolbar}>
+            <Hidden mdUp>
+              {!menuOpen && (
+                <HamburgerIcon
+                  onClick={() => setMenuOpen(prev => !prev)}
+                  className={classes.menuTrigger}
+                />
+              )}
+            </Hidden>
+            <Hidden smDown>
+              <nav>
+                <NavLinks active={classes.active} className={classes.anchor} />
+                <Button variant="contained" size="large" color="secondary">
+                  <Link to="contact" smooth={true} spy={true} offset={-150}>
+                    Let's Connect
+                  </Link>
+                </Button>
+              </nav>
+            </Hidden>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Fragment>
   );
 }
 
